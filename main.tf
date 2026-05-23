074c2f0e1f7cbd62c6f0e251506f20a2d5713043
Md Amir Irfan <amir123344irfan@gmail.com>
2026-05-23 16:57:18 +0530

    added PGA

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ resource "google_compute_subnetwork" "subnet" {
   name          = "${var.network_name}-subnet" # <-- Removed the backslash
   ip_cidr_range = var.subnet_cidr
   region        = var.region
+  private_ip_google_access   = true
   network       = google_compute_network.vpc.id
 }
 
