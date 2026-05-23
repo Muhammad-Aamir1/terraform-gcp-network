61b8ea3659492cf30b9c5277d8751bb2a0ebccdc
Md Amir Irfan <amir123344irfan@gmail.com>
2026-05-23 15:31:27 +0530

    feat: added manual subnets for v2.0.0 release

--- main.tf
+++ main.tf
@@ -1,10 +1,27 @@
-# Basic Network Module (v1.0.0)
+# # Basic Network Module (v1.0.0)
+# resource "google_compute_network" "vpc" {
+#   name                    = var.network_name
+#   auto_create_subnetworks = true
+# }
+
+# variable "network_name" {
+#   type        = string
+#   description = "The name of the VPC network"
+# }
+
+# Advanced Network Module (v2.0.0)
 resource "google_compute_network" "vpc" {
   name                    = var.network_name
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false # Team B requires manual subnet control
 }
 
-variable "network_name" {
-  type        = string
-  description = "The name of the VPC network"
+resource "google_compute_subnetwork" "subnet" {
+  name          = "\${var.network_name}-subnet"
+  ip_cidr_range = var.subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc.id
 }
+
+variable "network_name" { type = string }
+variable "subnet_cidr"  { type = string }
+variable "region"       { type = string }
